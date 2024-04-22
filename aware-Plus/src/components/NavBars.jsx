64909c9cad5a346019fa01a85cbe594a70ae0290
Nicolas Kraft <nkraft2018@fau.edu>
2024-04-22 14:21:21 -0400

    basic layout update
    + added simple search bar, home link, and log in link

--- aware-Plus/src/components/NavBars.jsx
+++ aware-Plus/src/components/NavBars.jsx
@@ -6,8 +6,25 @@ const HeaderBar = () => {
     return (
         <div className="header-bar">
             <header>
-                <nav>
-
+                <nav className="header-nav">
+                    <div className="header-home">
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </div>
+                    <div className="search-bar">
+                        <label htmlFor="Search" className="visually-hidden">Search awarePlus</label>
+                        <input
+                            type="text"
+                            placeholder="Search awarePlus"
+                            onChange={(e) => search(e.target.value)}
+                        />
+                    </div>
+                    <div className="header-login">
+                        <Link>
+                            Log In
+                        </Link>
+                    </div>
                 </nav>
             </header>
         </div>
@@ -19,7 +36,11 @@ const SideNav = () => {
     return (
         <div className="side-nav">
             <nav>
-
+                <div>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </div>
             </nav>
         </div>
     )
