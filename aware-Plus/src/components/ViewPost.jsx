61a85a3b7767c0a5a82caabb401865516860a2b0
Nicolas Kraft <nkraft2018@fau.edu>
2024-04-23 17:38:24 -0400

    Update ViewPost.jsx
    added comment section, displaying comments for each post
    
    and option to create new comment

--- aware-Plus/src/components/ViewPost.jsx
+++ aware-Plus/src/components/ViewPost.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef, useCallback} from "react";
 import {useParams} from "react-router-dom";
 import {supabase} from '../client.js';
 
@@ -6,7 +6,48 @@ const ViewPost = () => {
 
     const {id} = useParams();
     const [post, setPost] = useState({created_at: null, id: null, title: "", text: "", image_url: "", video_url: ""});
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
 
+    const textAreaRef = useRef(null);
+
+    // const fetchComments = useCallback(async () => {
+
+    //     const {data} = await supabase
+    //         .from('comments')
+    //         .select('*')
+    //         .eq('post_id', id);
+        
+    //     if (data){
+    //         console.log(data);
+    //         setComments(data);
+    //     } 
+    // }, [id, supabase])
+
+    // const fetchComments = async () => {
+
+    //     const { data } = await supabase
+    //         .from('comments')
+    //         .select('*')
+    //         .eq('post_id', id);
+
+    //     if (data) {
+    //         console.log(data);
+    //         setComments(data);
+    //     }
+    // }
+
+    // const fetchComments = async () => {
+    //     const { data } = await supabase
+    //         .from('comments')
+    //         .select('*');
+    
+    //     if (data) {
+    //         const filteredComments = data.filter(comment => comment.post_id === id);
+    //         setComments(filteredComments);
+    //     }
+    // }
+    
     useEffect(() => {
 
         const fetchPost = async () => {
@@ -21,24 +62,85 @@ const ViewPost = () => {
             }
         }
         fetchPost();
-    }, [id])
+
+        const fetchComments = async () => {
+
+            const { data } = await supabase
+                .from('comments')
+                .select('*')
+                .eq('post_id', id);
+
+            if (data) {
+                console.log(data);
+                setComments(data);
+            }
+        }
+        fetchComments();
+
+
+
+
+        // const subscription = supabase
+        //     .from('comments')
+        //     .on('*', payload => {
+        //         fetchComments();
+        //     })
+        //     .subscribe();
+
+        // // Unsubscribe when the component is unmounted
+        // return () => {
+        //     supabase.removeSubscription(subscription);
+        // }
+    }, [id, supabase])
 
     function renderVideo(url) {
 
         if (url.includes('youtube.com')) {
             const videoId = url.split('v=')[1];
             const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-            return <iframe src={embedUrl} frameborder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>;
+            return <iframe src={embedUrl} style={{border: 'none'}} allow="autoplay; encrypted-media" allowFullScreen></iframe>;
         }
         else if (url.includes('youtu.be')) {
             const videoId = url.split('/')[3].split('?')[0];
             const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-            return <iframe src={embedUrl} frameborder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>;
+            return <iframe src={embedUrl} style={{border: 'none'}} allow="autoplay; encrypted-media" allowFullScreen></iframe>;
         }
         else if (url.endsWith('.mp4')){
             return <video controls><source src={url} type="video/mp4"/></video>;
         }
     }
+
+    const adjustComment = (event) => {
+
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = "inherit";
+            textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+        }
+    }
+
+    const handleChange = (event) => {
+
+        setNewComment(event.target.value);
+    }
+
+    // const fetchCommentsRef = useRef(fetchComments);
+    // fetchCommentsRef.current = fetchComments;
+
+    const commentSubmit = async (event) => {
+
+        //event.preventDefault();
+        const {data} = await supabase
+        .from('comments')
+        .insert({post_id: id, comment: newComment});
+    
+        // if (data) {
+        //     //setComments([]);
+        //     // wait for insert op to complete before fetching comments
+        //     setTimeout(fetchComments, 1000);
+        // }
+        setNewComment('');
+        textAreaRef.current.value = "";
+    } 
 
     return (
         <div>
@@ -48,6 +150,22 @@ const ViewPost = () => {
             {post.image_url && <img src={post.image_url}/>}
             {post.video_url && renderVideo(post.video_url)}
             {/* {post.video_url && <video controls><source src={post.video_url} type="video/mp4"/></video>} */}
+            <div>
+                <h3>Comment Section</h3>
+                
+                <form>
+                    <textarea ref={textAreaRef} rows="1" cols="50" id="comment" placeholder="Add a comment .." onChange={(event) => {handleChange(event); adjustComment(event);}}></textarea>
+                    <button type="submit" onClick={commentSubmit}>Submit</button>
+                </form>
+                
+                <br/>
+                {comments && comments.length > 0 ?
+                comments.map((comment, index) => (
+                    <div key={comment.id}>
+                        <p>{comment.comment}</p>
+                    </div>
+                )): <div></div>}
+            </div>
         </div>
     )
 }
